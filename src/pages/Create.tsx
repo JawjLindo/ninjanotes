2df28c7424008569cc0943df39e967a11911f458
Mike Linde <mlinde@lintechsol.com>
2024-06-17 14:37:15 -0700

    Using react-hook-form

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -13,18 +13,19 @@ import {
   Typography,
 } from '@mui/material';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Notification } from '../components/Notification';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { NotesApi } from '../services';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+
+type FormInputs = {
+  title: string;
+  details: string;
+  category: string;
+};
 
 export const Create = () => {
-  const [title, setTitle] = useState('');
-  const [details, setDetails] = useState('');
-  const [titleError, setTitleError] = useState(false);
-  const [detailsError, setDetailsError] = useState(false);
-  const [category, setCategory] = useState('todos');
   const navigate = useNavigate();
   const styles: { field: TextFieldProps['sx'] | FormControlProps['sx'] } = {
     field: {
@@ -33,6 +34,12 @@ export const Create = () => {
       display: 'block',
     },
   };
+
+  const {
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm<FormInputs>();
 
   const queryClient = useQueryClient();
 
@@ -49,14 +56,12 @@ export const Create = () => {
     },
   });
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setTitleError(title == '' ? true : false);
-    setDetailsError(title == '' ? true : false);
-
-    if (title && details) {
-      createNote({ title, details, category });
-    }
+  const onSubmit: SubmitHandler<FormInputs> = (data) => {
+    createNote({
+      title: data.title,
+      details: data.details,
+      category: data.category,
+    });
   };
 
   return (
@@ -69,6 +74,12 @@ export const Create = () => {
       {createNoteError && (
         <Notification message={createNoteError.message} type='error' />
       )}
+      {errors.title && (
+        <Notification message={errors.title.message!} type='error' />
+      )}
+      {errors.details && (
+        <Notification message={errors.details.message!} type='error' />
+      )}
       <Typography
         variant='h6'
         component='h2'
@@ -78,58 +89,77 @@ export const Create = () => {
         Create a New Note
       </Typography>
 
-      <form noValidate autoComplete='off' onSubmit={handleSubmit}>
-        <TextField
-          onChange={(e) => setTitle(e.target.value)}
-          label='Note Title'
-          color='secondary'
-          fullWidth
-          required
-          sx={styles.field}
-          value={title}
-          error={titleError}
+      <form autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          control={control}
+          name='title'
+          defaultValue=''
+          rules={{ required: 'The title is required.' }}
+          render={({ field }) => (
+            <TextField
+              label='Note Title'
+              color='secondary'
+              fullWidth
+              required
+              sx={styles.field}
+              error={errors.title ? true : false}
+              {...field}
+            />
+          )}
         />
-        <TextField
-          onChange={(e) => setDetails(e.target.value)}
-          label='Details'
-          color='secondary'
-          fullWidth
-          multiline
-          rows={4}
-          required
-          sx={styles.field}
-          value={details}
-          error={detailsError}
+
+        <Controller
+          control={control}
+          name='details'
+          defaultValue=''
+          rules={{ required: 'The details are required.' }}
+          render={({ field }) => (
+            <TextField
+              label='Details'
+              color='secondary'
+              fullWidth
+              multiline
+              rows={4}
+              required
+              sx={styles.field}
+              error={errors.details ? true : false}
+              {...field}
+            />
+          )}
         />
 
         <FormControl sx={styles.field}>
           <FormLabel>Note Category</FormLabel>
 
-          <RadioGroup
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-          >
-            <FormControlLabel
-              value='money'
-              control={<Radio color='secondary' />}
-              label='Money'
-            />
-            <FormControlLabel
-              value='todos'
-              control={<Radio color='secondary' />}
-              label='Todos'
-            />
-            <FormControlLabel
-              value='reminders'
-              control={<Radio color='secondary' />}
-              label='Reminders'
-            />
-            <FormControlLabel
-              value='work'
-              control={<Radio color='secondary' />}
-              label='Work'
-            />
-          </RadioGroup>
+          <Controller
+            control={control}
+            name='category'
+            defaultValue='todos'
+            render={({ field }) => (
+              <RadioGroup {...field}>
+                <FormControlLabel
+                  value='money'
+                  control={<Radio color='secondary' />}
+                  label='Money'
+                />
+                <FormControlLabel
+                  value='todos'
+                  control={<Radio color='secondary' />}
+                  label='Todos'
+                />
+                <FormControlLabel
+                  value='reminders'
+                  control={<Radio color='secondary' />}
+                  label='Reminders'
+                />
+                <FormControlLabel
+                  value='work'
+                  control={<Radio color='secondary' />}
+                  label='Work'
+                />
+              </RadioGroup>
+            )}
+          />
         </FormControl>
 
         <Button
@@ -137,6 +167,7 @@ export const Create = () => {
           color='secondary'
           variant='contained'
           endIcon={<KeyboardArrowRightIcon />}
+          disabled={isPendingCreate}
         >
           Submit
         </Button>
